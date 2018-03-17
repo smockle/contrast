0a667b9b9f6ce9de53f2cbfc95fb6d266dc5aa8a
Clay Miller <clay@smockle.com>
2018-03-16 23:49:25 -0700

    Update batch file

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,7 +2,7 @@ REM Clean 'dist' directory
 rmdir /s /q "dist"
 
 REM Compile TypeScript using config in 'tsconfig.json'
-tsc -d
+call tsc -d
 
 REM Rename files in 'dist' to support ES modules with '.mjs'
-CALL "scripts\rename.bat"
+call "scripts\rename.bat"
