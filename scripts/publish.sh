72baf3f701aa8e433e8796b0b4c25034fb954cff
Clay Miller <clay@smockle.com>
2019-06-08 16:16:46 -0400

    Test publishing to GitHub Package Registry

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,11 +2,12 @@
 set -eo pipefail
 
 setup_npm() {
-  echo "//npm.pkg.github.com/:_authToken=${GITHUB_ACCESS_TOKEN}" >> .npmrc
+  echo "//npm.pkg.github.com/:_authToken=${GITHUB_ACCESS_TOKEN}" >> ~/.npmrc
+  echo "@smockle:registry=https://npm.pkg.github.com/" > .npmrc
 }
 
 publish_package() {
-  npm publish
+  npm publish --access public
 }
 
 setup_npm && publish_package
